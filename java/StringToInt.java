b178304dbd2809cc154059e03b244909cb3db679
Naman rai <namanrai309@gmail.com>
2022-10-30 21:32:57 +0530

    String To Int

---
+++ java/StringToInt.java
@@ -0,0 +1,9 @@
+public class StringToIntExample1{  
+public static void main(String args[]){  
+//Declaring String variable  
+String s="200";  
+//Converting String into int using Integer.parseInt()  
+int i=Integer.parseInt(s);  
+//Printing value of i  
+System.out.println(i);  
+}}  
