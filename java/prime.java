80614c9afa6540c16eb73a88d4d26e48ac18ab51
akkupy <akkupy@proton.me>
2022-10-30 14:51:30 +0530

    added Contributing.md

--- java/prime.java
+++ java/prime.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-public class prime {
-        public static void main(String args[]){
-                int a , b=0;
-		Scanner obj = new Scanner(System.in);
-		System.out.println("Enter a number :");
-		a =  obj.nextInt();
-                obj.close();
-                if (a==0 || a==1)
-                        System.out.println("Neither prime nor composite");
-                for (int i=2;i<=a/2;i++){
-                        if (a%i==0){
-                                b = 1;
-                                break;
-                        }       
-                }
-                if (b==1)
-                        System.out.println("It is Not Prime");
-                else 
-                        System.out.println("It is Prime");
-        }
-}
+class Prime
+{
+ public static void main(String arfgs[])
+ {int n,i,flag=0;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the no");
+  n=sc.nextInt();
+  if(n==0 || n==1)
+    System.out.println("not prime");
+  else
+  {for(i=2;i<=n/2;i++)
+    {if(n%i==0)
+       {flag=1;
+        System.out.println("not prime");  
+        break;
+       }
+       sc.close(); 
+    }
+  if(flag==0)
+  System.out.println("prime");
 
+}
+ 
+  }
+ }   
