471f878ebcb4822f66700062f5bac70003b26e8c
sanjanasajeev <76398738+sanjanasajeev@users.noreply.github.com>
2022-10-31 15:50:05 +0530

    Create Triangle.js

---
+++ JavaScript/Triangle.js
@@ -0,0 +1,9 @@
+const baseValue = prompt('Enter the base of a triangle: ');
+const heightValue = prompt('Enter the height of a triangle: ');
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
