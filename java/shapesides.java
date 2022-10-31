4dfce107a8d05854dbfc612c75d3e38114223052
vasilU <117096030+vasilU@users.noreply.github.com>
2022-10-31 20:10:10 +0530

    Add files via upload

---
+++ java/shapesides.java
@@ -0,0 +1,41 @@
+abstract class Shapesides
+{
+    void numberofSides(){}
+}
+
+class Rectangle extends Shape
+{
+    void numberofSides()
+    {
+        System.out.println("Rectangle has 4 sides.");
+    }
+}
+
+class Triangle extends Shape
+{
+    void numberofSides()
+    {
+    System.out.println("Triangle has 3 sides.");
+    }
+}
+
+class Hexagon extends Shape
+{
+    void numberofSides()
+    {
+    System.out.println("Hexagon has 6 sides.");
+    }
+}
+
+class shapesides
+{
+    public static void main(String[] args)
+    {
+    Rectangle r = new Rectangle();
+    r.numberofSides();
+    Triangle t = new Triangle();
+    t.numberofSides();
+    Hexagon h = new Hexagon();
+    h.numberofSides();
+    }
+}
