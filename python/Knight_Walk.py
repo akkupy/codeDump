8ec6c75cd41613ceb52e6f7cb105531518d4b943
techmaniac02 <117096012+techmaniac02@users.noreply.github.com>
2022-10-31 20:10:00 +0530

    Update Knight_Walk.py

--- python/Knight_Walk.py
+++ python/Knight_Walk.py
@@ -1,103 +1,88 @@
-// C++ program for Knight Tour problem
-#include <bits/stdc++.h>
-using namespace std;
+# Python3 program to solve Knight Tour problem using Backtracking
 
-#define N 8
+# Chessboard Size
+n = 8
 
-int solveKTUtil(int x, int y, int movei, int sol[N][N],
-				int xMove[], int yMove[]);
 
-/* A utility function to check if i,j are
-valid indexes for N*N chessboard */
-int isSafe(int x, int y, int sol[N][N])
-{
-	return (x >= 0 && x < N && y >= 0 && y < N
-			&& sol[x][y] == -1);
-}
+def isSafe(x, y, board):
+	'''
+		A utility function to check if i,j are valid indexes
+		for N*N chessboard
+	'''
+	if(x >= 0 and y >= 0 and x < n and y < n and board[x][y] == -1):
+		return True
+	return False
 
-/* A utility function to print
-solution matrix sol[N][N] */
-void printSolution(int sol[N][N])
-{
-	for (int x = 0; x < N; x++) {
-		for (int y = 0; y < N; y++)
-			cout << " " << setw(2) << sol[x][y] << " ";
-		cout << endl;
-	}
-}
 
-/* This function solves the Knight Tour problem using
-Backtracking. This function mainly uses solveKTUtil()
-to solve the problem. It returns false if no complete
-tour is possible, otherwise return true and prints the
-tour.
-Please note that there may be more than one solutions,
-this function prints one of the feasible solutions. */
-int solveKT()
-{
-	int sol[N][N];
+def printSolution(n, board):
+	'''
+		A utility function to print Chessboard matrix
+	'''
+	for i in range(n):
+		for j in range(n):
+			print(board[i][j], end=' ')
+		print()
 
-	/* Initialization of solution matrix */
-	for (int x = 0; x < N; x++)
-		for (int y = 0; y < N; y++)
-			sol[x][y] = -1;
 
-	/* xMove[] and yMove[] define next move of Knight.
-	xMove[] is for next value of x coordinate
-	yMove[] is for next value of y coordinate */
-	int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-	int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+def solveKT(n):
+	'''
+		This function solves the Knight Tour problem using
+		Backtracking. This function mainly uses solveKTUtil()
+		to solve the problem. It returns false if no complete
+		tour is possible, otherwise return true and prints the
+		tour.
+		Please note that there may be more than one solutions,
+		this function prints one of the feasible solutions.
+	'''
 
-	// Since the Knight is initially at the first block
-	sol[0][0] = 0;
+	# Initialization of Board matrix
+	board = [[-1 for i in range(n)]for i in range(n)]
 
-	/* Start from 0,0 and explore all tours using
-	solveKTUtil() */
-	if (solveKTUtil(0, 0, 1, sol, xMove, yMove) == 0) {
-		cout << "Solution does not exist";
-		return 0;
-	}
-	else
-		printSolution(sol);
+	# move_x and move_y define next move of Knight.
+	# move_x is for next value of x coordinate
+	# move_y is for next value of y coordinate
+	move_x = [2, 1, -1, -2, -2, -1, 1, 2]
+	move_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
-	return 1;
-}
+	# Since the Knight is initially at the first block
+	board[0][0] = 0
 
-/* A recursive utility function to solve Knight Tour
-problem */
-int solveKTUtil(int x, int y, int movei, int sol[N][N],
-				int xMove[8], int yMove[8])
-{
-	int k, next_x, next_y;
-	if (movei == N * N)
-		return 1;
+	# Step counter for knight's position
+	pos = 1
 
-	/* Try all next moves from
-	the current coordinate x, y */
-	for (k = 0; k < 8; k++) {
-		next_x = x + xMove[k];
-		next_y = y + yMove[k];
-		if (isSafe(next_x, next_y, sol)) {
-			sol[next_x][next_y] = movei;
-			if (solveKTUtil(next_x, next_y, movei + 1, sol,
-							xMove, yMove)
-				== 1)
-				return 1;
-			else
-				
-			// backtracking
-				sol[next_x][next_y] = -1;
-		}
-	}
-	return 0;
-}
+	# Checking if solution exists or not
+	if(not solveKTUtil(n, board, 0, 0, move_x, move_y, pos)):
+		print("Solution does not exist")
+	else:
+		printSolution(n, board)
 
-// Driver Code
-int main()
-{
-	// Function Call
-	solveKT();
-	return 0;
-}
 
+def solveKTUtil(n, board, curr_x, curr_y, move_x, move_y, pos):
+	'''
+		A recursive utility function to solve Knight Tour
+		problem
+	'''
+
+	if(pos == n**2):
+		return True
+
+	# Try all next moves from the current coordinate x, y
+	for i in range(8):
+		new_x = curr_x + move_x[i]
+		new_y = curr_y + move_y[i]
+		if(isSafe(new_x, new_y, board)):
+			board[new_x][new_y] = pos
+			if(solveKTUtil(n, board, new_x, new_y, move_x, move_y, pos+1)):
+				return True
+
+			# Backtracking
+			board[new_x][new_y] = -1
+	return False
+
+
+# Driver Code
+if __name__ == "__main__":
+	
+	# Function Call
+	solveKT(n)
 
