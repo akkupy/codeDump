20cfeb9b7ee49da322577bf97d20ff74670ba0f3
Ankush Tripathi <89534251+ankushtripathii@users.noreply.github.com>
2023-10-01 11:34:01 +0530

    Update and rename Maximum Value of an Ordered Triplet I to Maximum Value of an Ordered Triplet I.java

--- java/Maximum Value of an Ordered Triplet I.java
+++ java/Maximum Value of an Ordered Triplet I.java
@@ -1,9 +1,8 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
        
-         int n = nums.length;
+        int n = nums.length;
         long maxVal = 0;
-        
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
@@ -12,9 +11,6 @@ class Solution {
                 }
             }
         }
-        
         return maxVal;
-        
-        
     }
 }
