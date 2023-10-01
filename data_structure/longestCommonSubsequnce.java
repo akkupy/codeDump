9dc32a345ffce5c64e51848f093c000e3a3d5a58
iKoNFlair <ritikpillai8@gmail.com>
2023-10-01 18:39:53 +0530

    Added Longest Commong Subsequnce

---
+++ data_structure/longestCommonSubsequnce.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+class Solution {
+    public int longestCommonSubsequence(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = 0;
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s1, s2;
+
+        Scanner scanner = new Scanner(System.in);
+        s1 = scanner.nextLine();
+        s2 = scanner.nextLine();
+
+        int result = sol.longestCommonSubsequence(s1, s2);
+        System.out.println(result);
+    }
+}
