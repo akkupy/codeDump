e8f49a24d020d186845a2ba901226fb099c0056a
vismaya2002 <vismay2002@gmail.com>
2023-10-02 10:48:44 +0530

    changed variable name

--- python/gcd.py
+++ python/gcd.py
@@ -1,9 +1,9 @@
 def gcd(x, y):
     if x > y:
-        smaller = y
+        small = y
     else:
-        smaller = x
-    for i in range(1, smaller+1):
+        small = x
+    for i in range(1, small+1):
         if((x % i == 0) and (y % i == 0)):
             hcf = i 
     return hcf
