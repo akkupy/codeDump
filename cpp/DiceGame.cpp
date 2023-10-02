14ada2be50207123996d2e71d5c7463af8da9af4
Vaibhav <82583704+hellovaibhav@users.noreply.github.com>
2023-10-02 10:34:17 +0530

    #fixes issue 521
    added dice game files in cpp

---
+++ cpp/DiceGame.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv) {
+   cout << "Enter the number of dice: ";
+   int n;
+   cin >> n;
+   cout << "The values on dice are: ";
+   for (int i = 0; i < n; i++)
+      cout << (rand() % 6) + 1<<" ";
+}
